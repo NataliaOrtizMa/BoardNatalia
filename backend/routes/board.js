90b437a8ab34b34862de7adf4f323757f5013f25
NataliaOrtizMa <nataliaortizma@gmail.com>
2021-05-27 15:42:45 -0500

    Feat: Routes edit task and delete task

--- backend/routes/board.js
+++ backend/routes/board.js
@@ -22,6 +22,27 @@ router.get("/getTasks", Auth, async(req,res) => {
     if (!user) return res.status(401).send("La persona no existe en DB");
     const board = await Board.find({userId: req.user._id});
     return res.status(200).send({board});
+});
+
+router.put("/editTask", Auth, async(req, res) =>{
+    const user = await User.findById(req.user._id);
+    if(!user) return res.status(401).send("No existe el usuario");
+    const board = await Board.findByIdAndUpdate(req.body._id, {
+        userId: user._id,
+        name: req.body.name,
+        description: req.body.description,
+        status: req.body.status,
+    })
+    if(!board) return res.status(401).send("No existe la tarea");
+    return res.status(200).send({board});
+});
+
+router.delete("/:_id", Auth, async(req, res) =>{
+    const user = await User.findById(req.user._id);
+    if (!user) return res.status(401).send("No existe el usuario");
+    const board = await Board.findByIdAndDelete(req.params._id);
+    if(!board) return res.status(401).send("No hay tarea para eliminar");
+    return res.status(200).send("Actividad eliminada");
 })
 
 module.exports = router;
